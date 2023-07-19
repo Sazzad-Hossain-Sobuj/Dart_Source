bd0f5a30ad2d4a1758b883f693d84fba4b035bfc
Sazzad-Hossain-Sobuj <sazzad15-2596@diu.edu.bd>
2023-07-19 11:05:53 +0600

    dart vaiables

---
+++ bin/variables/main.dart
@@ -0,0 +1,15 @@
+void main() {
+  var fname = 'Sazzad';
+  var lname = 'Hossain';
+  print(fname+' '+lname);
+
+  String firstname = 'S';
+  String lastname = 'Hossain';
+  print(firstname + ' ' + lastname);
+
+  int a =10;
+  int b = 20;
+  print(a+b);
+
+  
+}
